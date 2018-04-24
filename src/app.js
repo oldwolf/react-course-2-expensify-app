9c36fed14e96b25afe4bd7ee77cc4f2f5c369083
Oldwolf Pang <bypang1226@gmail.com>
2018-04-24 22:11:10 +0800

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
 
 const jsx = (
   <Provider store={store}>
