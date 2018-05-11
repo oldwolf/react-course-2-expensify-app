3a0191ba5f9c666506e94a3d8d711cb54ddad2f1
Oldwolf Pang <bypang1226@gmail.com>
2018-05-11 17:44:52 +0800

    Add Expensify styles

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { connect }  from  'react-redux';
+import { connect } from  'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 import pluralize from 'pluralize';
 
-export const ExpenseSummary = (props) => (
-  <div>
-    <h1>Viewing {props.expenses.length} {pluralize('expense', props.expenses.length)} totalling {numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}</h1>
-  </div>
-);
+export const ExpenseSummary = ({ expenseCount, expenseTotal }) => {
+  const formattedExpenseTotal = numeral(expenseTotal / 100).format('$0,0.00');
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {pluralize('expense', expenseCount)} totalling <span>{formattedExpenseTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
+  const visibleExpense = selectExpenses(state.expenses, state.filters);
+
   return {
-    expenses: selectExpenses(state.expenses, state.filters)
+    expenseCount: visibleExpense.length,
+    expenseTotal: selectExpensesTotal(visibleExpense)
   };
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => {
-  return (
-    <div>
-      <button onClick={startLogin}>Login</button>
+export const LoginPage = ({ startLogin }) => (
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box_layout__title">Expensify App</h1>
+        <p>It's time to get your expenses under contorl.</p>
+        <button
+          className="button"
+          onClick={startLogin}
+        >
+          Login with Google
+        </button>
+      </div>
     </div>
-  );
-};
+);
 
 const mapDispatchToProps = (dispatch) => ({
   startLogin: () => dispatch(startLogin())
